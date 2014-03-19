d78a047579ef383bc52bbbf7560a1359b0da97f9
songkrit <songkritr@gmail.com>
2014-03-19 16:45:51 +0700

    listview example

--- src/com/example/listview/MainActivity.java
+++ src/com/example/listview/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.listview;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private ListView listview;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		listview = (ListView) this.findViewById(R.id.listView1);
+		String[] values = new String[] { "Android List View", 
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android", 
+                "Android Example", 
+                "List View Source Code", 
+                "List View Array Adapter", 
+                "Android Example List View" 
+               };
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+	              android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+		listview.setAdapter(adapter);
 	}
 
 	@Override
